7fe72eb4ed39d3025aaac5d3917c970d6d9c7bc6
IvanovGoat <mitri4.121@gmail.com>
2025-12-18 17:33:35 +0300

    final version

--- test_Project_1.py
+++ test_Project_1.py
@@ -6,18 +6,18 @@ def test_get_random_word_length():
     assert 4 <= len(word) <= 8
 
 
-def test_get_random_word_is_lowercase():
+def test_get_random_word_lower():
     word = Project_1.get_random_word()
     assert word == word.lower()
 
 
-def test_get_random_word_in_word_list():
+def test_get_random_word_in_list():
     word = Project_1.get_random_word()
     original_words_lower = [w.lower() for w in Project_1.WORDS]
     assert word in original_words_lower
 
 
-def test_get_random_word_empty_valid_list_raises_value_error():
+def test_get_random_word_negative():
     original = Project_1.WORDS.copy()
     Project_1.WORDS = ["а", "бы", "в"]
     try:
@@ -29,9 +29,9 @@ def test_get_random_word_empty_valid_list_raises_value_error():
         Project_1.WORDS[:] = original
 
 
-def test_alphabet_has_33_letters():
+def test_alphabet_33_letters():
     assert len(Project_1.ALPHABET) == 33
 
 
-def test_alphabet_letters_are_unique():
+def test_alphabet_unique():
     assert len(Project_1.ALPHABET) == len(set(Project_1.ALPHABET))
